5e40369f4efc103d9a81c0164316dae667c87efe
garrett-virtualitics <garrett@virtualitics.com>
2017-09-13 16:31:39 -0700

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -2,6 +2,14 @@
 
 set PUBLISH_DIR=C:\Users\Virtualitics\Desktop\WorkingFolder\test
 
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
+
+echo.
+echo ******************************************************
+echo Publishing to Dropbox...
+mkdir !PUBLISH_DIR!\"%mydate%_%mytime%"
+
 xcopy /E /y /q /f /s /i .\Buildtest !PUBLISH_DIR!\"%mydate%_%mytime%"
 
 echo.
