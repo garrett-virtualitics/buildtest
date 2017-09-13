ec0d9eafdf6b77425e5f17485c707186ee1cff5c
garrett-virtualitics <garrett@virtualitics.com>
2017-09-13 16:27:59 -0700

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -1,12 +1,9 @@
 @echo off
 
-set BUILD_DIR=C:\Users\Virtualitics\Documents\GitHub\buildtest
 set PUBLISH_DIR=C:\Users\Virtualitics\Desktop\WorkingFolder\test
 
-xcopy /E /y /q /f /s /i !BUILD_DIR!\BuildTest !PUBLISH_DIR!\"%mydate%_%mytime%"
+xcopy /E /y /q /f /s /i .\Buildtest !PUBLISH_DIR!\"%mydate%_%mytime%"
 
 echo.
 echo Publishing complete.
 echo.
-
-pause
