2d9f63e3f88aeeb862604d451063f254806335c5
garrett-virtualitics <garrett@virtualitics.com>
2017-09-13 17:27:30 -0700

    Added dummy project

---
+++ Buildtest/Buildtest/Assets/Editor/CmdBuild.cs
@@ -0,0 +1,25 @@
+﻿// Workaround - unity cmd line build options do not allow enabling development mode
+//
+// To use from cmd line:
+//
+// PATH/Unity.exe -batchmode -projectPath PATH -executeMethod CmdBuild.DevBuild -quit
+
+
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+using System.Collections;
+using System.Linq;
+
+public class CmdBuild : MonoBehaviour
+{
+    private static string[] EnabledScenes()
+    {
+        return (from scene in EditorBuildSettings.scenes where scene.enabled select scene.path).ToArray();
+    }
+
+    public static void DevBuild()
+    {
+        BuildPipeline.BuildPlayer(EnabledScenes(), "iViz_Build.exe", BuildTarget.StandaloneWindows64, BuildOptions.Development);
+    }
+}
